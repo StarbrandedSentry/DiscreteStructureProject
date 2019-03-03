50a128445fd7d1bfaf9f3662e51ab232e81992d5
StarbrandedSentry <hierarchhero@gmail.com>
2019-03-04 07:58:38 +0800

    first update
    almost done tho

--- app/src/main/java/com/iibcsad/recursion/activities/FermatActivity.java
+++ app/src/main/java/com/iibcsad/recursion/activities/FermatActivity.java
@@ -1,9 +1,11 @@
 package com.iibcsad.recursion.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -28,33 +30,62 @@ public class FermatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fermat);
+
+        inputIndex = findViewById(R.id.a_fermat_input);
+        numbers = new ArrayList<>();
+
+        calculate = findViewById(R.id.a_fermat_calculate);
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(inputIndex.getText().toString().matches(""))
+                {
+                    Snackbar.make(v, "Input something!", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                else if(isPrime(Double.parseDouble(inputIndex.getText().toString())))
+                {
+                    Snackbar.make(v, "Input number already a prime number!", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                numbers.clear();
+                calculateUntilIndex(Double.parseDouble(inputIndex.getText().toString()));
+            }
+        });
     }
 
-    private void calculateUntilIndex(double index)
+    private void calculateUntilIndex(double num)
     {
         NumberFormat nb = new DecimalFormat("#0");
-        series = "";
-        pure = "";
-        double end;
-        int count = 0;
         boolean flag = false;
+        Double N = num;
+        double x, y;
+        String xS, yS;
         //Toast.makeText(getApplicationContext(), isPrime(index) + "", Toast.LENGTH_SHORT).show();
+
+        Double nN = Math.sqrt(N.intValue());
+        int n = nN.intValue();
+        if(n < Math.sqrt(N.intValue()))
+        {
+            n++;
+        }
+        N = Double.valueOf(n);
 
         while (!flag)
         {
-            if(isPrime(index))
+            numbers.add(n + " - " + nb.format(num) + " = " + ((n*n)-num));
+
+            if(Math.sqrt(((N * N) - num)) % 1 == 0)
             {
-                /*pure = nb.format(index) + pure;
-                numbers.add(pure);*/
+                //Toast.makeText(getApplicationContext(), " - " + n, Toast.LENGTH_SHORT).show();
+                x = n  - Math.sqrt(((N * N) - num));
+                y = n  + Math.sqrt(((N * N) - num));
+                numbers.add("FACTORS:(" + nb.format(x) + "), (" + nb.format(y) + ")");
                 break;
             }
-            else
-            {
-                series = nb.format(index / divisibleBy(index)) + pure+ " * " + nb.format(divisibleBy(index));
-                pure +=  " * " + nb.format(divisibleBy(index));
-                numbers.add(series);
-                index = index / divisibleBy(index);
-            }
+
+            N++;
+            n++;
         }
 
 
@@ -63,7 +94,7 @@ public class FermatActivity extends AppCompatActivity {
 
     private void initRecyclerView()
     {
-        recyclerView = findViewById(R.id.a_prime_view);
+        recyclerView = findViewById(R.id.a_fermat_view);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerAdapter = new SequenceAdapter(numbers);
